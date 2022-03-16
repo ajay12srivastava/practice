9526e53eeec02d42575a31f0c04d6cbe2218628e
Ajay Kumar Shrivastava <ajayshrivastava@in.ibm.com>
2022-03-16 22:00:09 +0000

    adding existing files to repo

---
+++ src/com/aj/string/palindrome/MergeTwoSortedSinglyLists.java
@@ -0,0 +1,28 @@
+package com.aj.string.palindrome;
+
+import java.util.LinkedList;
+
+import com.aj.util.ListNode;
+
+/**
+ * 21. Merge Two Sorted Singly Lists (LeetCode)
+ * @author ajay
+ *
+ */
+
+public class MergeTwoSortedSinglyLists {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        if(list1.)
+    }
+	
+
+	
+	
+}
